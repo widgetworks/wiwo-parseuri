4ac2263b1dcba74755652fef6a91dd33f6741f50
Nathaniel Fitzgerald-Hood <nathanielf@gmail.com>
2014-10-15 16:28:59 +1000

    feat(ts): add TypeScript descriptor

---
+++ parseuri.d.ts
@@ -0,0 +1,34 @@
+declare class URI {
+    static parse(url:string): IUriDescriptor;
+    static options: {
+        strict: boolean;
+    };
+}
+
+/**
+ * Result from URI.parse
+ *
+ * https://github.com/widgetworks/parseuri
+ */
+declare interface IUriDescriptor {
+    anchor: string;
+    query: string;
+    file: string;
+    directory: string;
+    path: string;
+    relative: string;
+    port: string;
+    host: string;
+    password: string;
+    user: string;
+    userInfo: string;
+    authority: string;
+    protocol: string;
+    source: string;
+    queryKey: any; //map of strings, keys are query string keys
+
+    toAbsolute(pattern: IUriDescriptor): string;
+    isAbsolute(): boolean;
+    isRelative(): boolean;
+    toString(except: string[]): string;
+}
